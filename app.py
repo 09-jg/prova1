35de08128f83fedbb06fa362624c31a90fa60483
09-jg <gabrielmenezesjoao6@gmail.com>
2024-10-06 19:49:37 -0300

    first commit

--- app.py
+++ app.py
@@ -1,38 +1,127 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///escola.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Aluno(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+
+class Sala(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    numero = db.Column(db.String(10), nullable=False)
+
+class Aula(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    aluno_id = db.Column(db.Integer, db.ForeignKey('aluno.id'), nullable=False)
+    sala_id = db.Column(db.Integer, db.ForeignKey('sala.id'), nullable=False)
+    data = db.Column(db.String(20), nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
-def home():
-    return render_template('bem_vindo.html')
+def index():
+    return render_template('index.html')
 
-@app.route('/cadastro', methods=['GET', 'POST'])
-def cadastro():
+# Alunos
+@app.route('/alunos', methods=['GET', 'POST'])
+def gerenciar_alunos():
     if request.method == 'POST':
         nome = request.form['nome']
-        data_nascimento = request.form['data_nascimento']
-        idade = request.form['idade']
-        endereco = request.form['endereco']
+        novo_aluno = Aluno(nome=nome)
+        db.session.add(novo_aluno)
+        db.session.commit()
+        return redirect(url_for('gerenciar_alunos'))
+
+    alunos = Aluno.query.all()
+    return render_template('alunos.html', alunos=alunos)
+
+@app.route('/alunos/edit/<int:id>', methods=['GET', 'POST'])
+def editar_aluno(id):
+    aluno = Aluno.query.get(id)
+    if request.method == 'POST':
+        aluno.nome = request.form['nome']
+        db.session.commit()
+        return redirect(url_for('gerenciar_alunos'))
+    return render_template('editar_aluno.html', aluno=aluno)
+
+@app.route('/alunos/delete/<int:id>')
+def deletar_aluno(id):
+    aluno = Aluno.query.get(id)
+    db.session.delete(aluno)
+    db.session.commit()
+    return redirect(url_for('gerenciar_alunos'))
+
+# Salas
+@app.route('/salas', methods=['GET', 'POST'])
+def gerenciar_salas():
+    if request.method == 'POST':
         numero = request.form['numero']
-        
-        # Validação da data
-        try:
-            datetime.strptime(data_nascimento, '%Y-%m-%d')
-        except ValueError:
-            flash("A data de Nascimento deve estar no formato YYYY-MM-DD")
-            return redirect(url_for('cadastro'))
-        
-        # Aqui você pode adicionar a lógica para salvar os dados no banco de dados
-        flash("Cadastro realizado com sucesso!")
-        return redirect(url_for('sucesso'))
+        nova_sala = Sala(numero=numero)
+        db.session.add(nova_sala)
+        db.session.commit()
+        return redirect(url_for('gerenciar_salas'))
 
-    return render_template('homepage.html')
+    salas = Sala.query.all()
+    return render_template('salas.html', salas=salas)
 
-@app.route('/sucesso')
-def sucesso():
-    return render_template('sucesso.html')
+@app.route('/salas/edit/<int:id>', methods=['GET', 'POST'])
+def editar_sala(id):
+    sala = Sala.query.get(id)
+    if request.method == 'POST':
+        sala.numero = request.form['numero']
+        db.session.commit()
+        return redirect(url_for('gerenciar_salas'))
+    return render_template('editar_sala.html', sala=sala)
 
-if __name__ == "__main__":
+@app.route('/salas/delete/<int:id>')
+def deletar_sala(id):
+    sala = Sala.query.get(id)
+    db.session.delete(sala)
+    db.session.commit()
+    return redirect(url_for('gerenciar_salas'))
+
+# Aulas
+@app.route('/aulas', methods=['GET', 'POST'])
+def gerenciar_aulas():
+    if request.method == 'POST':
+        aluno_id = request.form['aluno_id']
+        sala_id = request.form['sala_id']
+        data = request.form['data']
+        nova_aula = Aula(aluno_id=aluno_id, sala_id=sala_id, data=data)
+        db.session.add(nova_aula)
+        db.session.commit()
+        return redirect(url_for('gerenciar_aulas'))
+
+    alunos = Aluno.query.all()
+    salas = Sala.query.all()
+    aulas = Aula.query.all()
+    return render_template('aulas.html', aulas=aulas, alunos=alunos, salas=salas)
+
+@app.route('/aulas/edit/<int:id>', methods=['GET', 'POST'])
+def editar_aula(id):
+    aula = Aula.query.get(id)
+    if request.method == 'POST':
+        aula.aluno_id = request.form['aluno_id']
+        aula.sala_id = request.form['sala_id']
+        aula.data = request.form['data']
+        db.session.commit()
+        return redirect(url_for('gerenciar_aulas'))
+    
+    alunos = Aluno.query.all()
+    salas = Sala.query.all()
+    return render_template('editar_aula.html', aula=aula, alunos=alunos, salas=salas)
+
+@app.route('/aulas/delete/<int:id>')
+def deletar_aula(id):
+    aula = Aula.query.get(id)
+    db.session.delete(aula)
+    db.session.commit()
+    return redirect(url_for('gerenciar_aulas'))
+
+if __name__ == '__main__':
     app.run(debug=True)
